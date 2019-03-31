f3330b46bd31911d48d63d6584a67b5eb27fb889
Antoine POUCHET <apouchet@e2r12p19.42.fr>
2019-03-31 20:41:45 +0200

    cursor menu and scene1

---
+++ Assets/Scripts/mouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class mouse : MonoBehaviour {
+
+    public Texture2D cursor;
+
+    void Start ()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+	}
+}
