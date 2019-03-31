718e957ebb5fab47fc766c10157556862a54004b
Emilien Baudet <emilien.baudet@gmail.com>
2019-03-31 04:40:22 +0200

    show weapon info on screen

---
+++ Assets/Scripts/PlayerWeaponInformation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWeaponInformation : MonoBehaviour {
+
+	public enum typeInfo {
+		weapon_name,
+		weapon_single
+	}
+	public typeInfo 	info;
+	public player		player;
+	protected Text 		text; 
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (info == typeInfo.weapon_name)
+		{
+			if (null == player.weapon)
+				text.text = "No Weapon";
+			else
+				text.text = player.weapon.name;
+		}
+		else if (info == typeInfo.weapon_single)
+		{
+			if (null == player.weapon)
+				text.text = "-";
+			else
+			{
+				if (player.weapon.inf_ammo)
+					text.text = "Inf Single";
+				else
+					text.text = player.weapon.ammo.ToString() + " Single";
+			}
+		}
+	}
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,57 +5,32 @@ using UnityEditor;
 using System.Text.RegularExpressions;
 
 public class player : MonoBehaviour {
+    public Camera       cam;
+    public GameObject   Player;
+    public GameObject   weapon_sprite;
+    public Weapon       weapon = null;
+    public float        speed;
 
-    public Camera cam;
-    public GameObject Player;
-    public GameObject weapon_sprite;
-    private GameObject weapon;
-    public float speed;
-    private string path;
-    private string weapon_name;
-    private string weapon_tmp;
-    private float vertical;
-    private float horizontal;
-    private Vector3 cameraDif;
-    private Vector3 direction;
-    private GameObject weapon_collision;
-    private Collider2D take = null;
+    private Vector3     direction;
+    private Collider2D  take = null;
 
 	// Use this for initialization
 	void Start () {
-        //cameraDif = cam.transform.position.y - transform.position.y;
-        //weapon_name = int.Parse(weapon_tmp);
-        //weapon_sprite = GetComponent<SpriteRenderer>();
-        //Debug.Log(we)
 
-        //path = "Assets/Sprites/weapons/attach-to-body/" + numbersOnly + ".png";
-        //Debug.Log(path);
-        //weapon_sprite.GetComponent<SpriteRenderer>().sprite = AssetDatabase.LoadAssetAtPath<SpriteRenderer>("Assets/Sprites/weapons/attach-to-body/1.png", typeof(SpriteRenderer));
-        //weapon_sprite.sprite = (Sprite)AssetDatabase.LoadAssetAtPath(path, typeof(Sprite));
-        //AssetDatabase.ImportAsset("Assets/Sprites/weapons/attach-to-body/1.png", ImportAssetOptions.Default);
-        //Application.
-                   //weapon_sprite = 
 	}
 
+    // Key Getters
     private bool getKeyUp()
-    {
-        return (Input.GetKey(KeyCode.W) || Input.GetKey("up"));
-    }
+    { return (Input.GetKey(KeyCode.W) || Input.GetKey("up")); }
 
     private bool getKeyDown()
-    {
-        return (Input.GetKey(KeyCode.S) || Input.GetKey("down"));
-    }
+    { return (Input.GetKey(KeyCode.S) || Input.GetKey("down")); }
 
     private bool getKeyRight()
-    {
-        return (Input.GetKey(KeyCode.D) || Input.GetKey("right"));
-    }
-
+    { return (Input.GetKey(KeyCode.D) || Input.GetKey("right")); }
+    
     private bool getKeyLeft()
-    {
-        return (Input.GetKey(KeyCode.A) || Input.GetKey("left"));
-    }
+    { return (Input.GetKey(KeyCode.A) || Input.GetKey("left")); }
 	
 	// Update is called once per frame
 	void Update ()
@@ -65,8 +40,8 @@ public class player : MonoBehaviour {
         else if (weapon && Input.GetMouseButtonDown(0))
             weapon.gameObject.GetComponent<Weapon>().fire(transform.position, transform.rotation);
                 
-        vertical = 0;
-        horizontal = 0;
+        float vertical = 0;
+        float horizontal = 0;
         if (getKeyDown())
             vertical += -0.2f;
         if (getKeyUp())
@@ -75,9 +50,7 @@ public class player : MonoBehaviour {
             horizontal += -0.2f;
         if (getKeyRight())
             horizontal += 0.2f;
-        // GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed * Time.deltaTime);
         GetComponent<Rigidbody2D>().velocity = new Vector2(speed * horizontal, speed * vertical);
-        // transform.Translate(horizontal, vertical, 0);
         if (weapon)
         {
             weapon.transform.position = new Vector3(transform.position.x - 0.2f, transform.position.y - 0.3f, 0);
@@ -117,8 +90,8 @@ public class player : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.E) && take.gameObject.tag == "weapon")
         {
             Debug.Log("ok");
-            weapon = take.gameObject;
-            path = "Assets/Sprites/weapons/attach-to-body/" + weapon.GetComponent<Weapon>().weapon_number + ".png";
+            weapon = take.GetComponent<Weapon>();
+            string path = "Assets/Sprites/weapons/attach-to-body/" + weapon.GetComponent<Weapon>().weapon_number + ".png";
             //Debug.Log(path);
 
             weapon_sprite.gameObject.SetActive(true);
@@ -135,51 +108,10 @@ public class player : MonoBehaviour {
         take = collision;
 	}
 
-
-
-	//private void OnTriggerStay2D(Collider2D collision)
-	//{
- //       Debug.Log("cocou");
- //       //Debug.Log(collision.gameObject.tag);
- //       if (Input.GetKeyDown(KeyCode.E) && collision.gameObject.tag == "weapon")
- //       {
- //           Debug.Log("ok");
- //           weapon = collision.gameObject;
- //           path = "Assets/Sprites/weapons/attach-to-body/" + weapon.GetComponent<Weapon>().weapon_number + ".png";
- //           //Debug.Log(path);
-
- //           weapon_sprite.gameObject.SetActive(true);
- //           weapon_sprite.GetComponent<SpriteRenderer>().sprite = (Sprite)AssetDatabase.LoadAssetAtPath(path, typeof(Sprite));
-
- //           weapon.GetComponent<CircleCollider2D>().enabled = false;
- //           weapon.GetComponent<SpriteRenderer>().enabled = false;
- //       }
-	//}
-
-	private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
 	{
         if (!weapon)
             other.GetComponent<BoxCollider2D>().enabled = true;
         take = null;
 	}
-
-	//public void OnCollisionEnter2D(Collision2D collision)
-	//{
-	//    //Debug.Log("coucou");
-	//    Debug.Log(collision.gameObject.tag);
-	//    if (Input.GetKeyDown(KeyCode.E) && collision.gameObject.tag == "weapon")
-	//    {
-	//        Debug.Log("ok");
-	//        weapon = collision.gameObject;
-	//        path = "Assets/Sprites/weapons/attach-to-body/" + weapon.GetComponent<Weapon>().weapon_number + ".png";
-	//        Debug.Log(path);
-	//        //weapon.GetComponent<SpriteRenderer>().sprite = (Sprite)AssetDatabase.LoadAssetAtPath(path, typeof(Sprite));
-	//        //string numbersOnly = Regex.Replace(weapon_tmp, "[^0-9]", "");
-	//        weapon.GetComponent<BoxCollider2D>().enabled = false;
-	//        weapon.GetComponent<SpriteRenderer>().enabled = false;
-	//        weapon_sprite.gameObject.SetActive(true);
-	//        weapon_sprite.GetComponent<SpriteRenderer>().sprite = (Sprite)AssetDatabase.LoadAssetAtPath(path, typeof(Sprite));
-	//    }
-	//}
-
 }
