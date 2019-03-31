413ecbbfb595d50fbe123308d11310fda6996a53
Antoine POUCHET <apouchet@e2r12p19.42.fr>
2019-03-31 23:33:53 +0200

    end

--- Assets/Scripts/end_menu.cs
+++ Assets/Scripts/end_menu.cs
@@ -5,28 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class end_menu : MonoBehaviour
 {
-	private void Update()
-	{
-        Debug.Log("end");
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Debug.Log("text = " + transform.Find("exit").name);
-            transform.Find("exit_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 40;
-            transform.Find("retry_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 30;
-        }
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.Find("exit_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 30;
-            transform.Find("retry_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 40;
-        }
-	}
-	public void replay()
-    {
-        SceneManager.LoadScene(1);
-    }
+	//private void Update()
+	//{
+ //       Debug.Log("end");
+ //       if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+ //       {
+ //           Debug.Log("text = " + transform.Find("exit").name);
+ //           transform.Find("exit_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 40;
+ //           transform.Find("retry_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 30;
+ //       }
+ //       else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+ //       {
+ //           transform.Find("exit_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 30;
+ //           transform.Find("retry_txt").GetComponent<UnityEngine.UI.Text>().fontSize = 40;
+ //       }
+	//}
+	//public void replay()
+    //{
+    //    SceneManager.LoadScene(1);
+    //}
 
-    public void exit()
-    {
-        Application.Quit();
-    }
+    //public void exit()
+    //{
+    //    Application.Quit();
+    //}
 }
