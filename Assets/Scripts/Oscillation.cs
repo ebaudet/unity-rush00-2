19287284882e128ee21b0ecad937a39d2408dfad
Cedric VAUTRAIN <cvautrai@e2r2p11.42.fr>
2019-05-25 20:38:37 +0200

    remove musics and rotate titles

---
+++ Assets/Scripts/Oscillation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillation : MonoBehaviour {
+
+	public int factor;
+	public bool inverseDir;
+	private int _direction = 1;
+
+	private void Start()
+	{
+		if (inverseDir)
+			_direction = -1;
+	}
+
+	private void Update ()
+	{
+		if (isWithin(transform.localEulerAngles.z, factor, 180))
+			_direction = -1;
+
+		if (isWithin(transform.localEulerAngles.z, 180, 360 - factor))
+			_direction = 1;
+		
+		transform.Rotate((Vector3.forward * _direction) * factor * Time.deltaTime);
+	}
+
+	private bool isWithin(float value, float min, float max)
+	{
+		if (value >= min && value <= max)
+			return true;
+		return false;
+	}
+}
