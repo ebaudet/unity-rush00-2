1f7364d96efba57906dc4838a7ee77d8eb1d2dfa
Emilien BAUDET <ebaudet@e2r2p13.42.fr>
2019-05-26 14:09:27 +0200

    put menu in scene 2 + correct player weapon information

--- Assets/Scripts/PlayerWeaponInformation.cs
+++ Assets/Scripts/PlayerWeaponInformation.cs
@@ -3,41 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerWeaponInformation : MonoBehaviour {
+public class PlayerWeaponInformation : MonoBehaviour
+{
+    public enum typeInfo
+    {
+        weapon_name,
+        weapon_single
+    }
+    public typeInfo info;
+    public player _player;
+    protected Text text;
 
-	public enum typeInfo {
-		weapon_name,
-		weapon_single
-	}
-	public typeInfo 	info;
-	public player		player;
-	protected Text 		text; 
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        _player = player.instance;
+    }
 
-	// Use this for initialization
-	void Start () {
-		text = GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (info == typeInfo.weapon_name)
-		{
-			if (null == player.weapon)
-				text.text = "No Weapon";
-			else
-				text.text = player.weapon.weapon_name;
-		}
-		else if (info == typeInfo.weapon_single)
-		{
-			if (null == player.weapon)
-				text.text = "-";
-			else
-			{
-				if (player.weapon.inf_ammo)
-					text.text = "Inf Single";
-				else
-					text.text = player.weapon.ammo.ToString() + " Single";
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (info == typeInfo.weapon_name)
+        {
+            if (null == _player.weapon)
+                text.text = "No Weapon";
+            else
+                text.text = _player.weapon.weapon_name;
+        }
+        else if (info == typeInfo.weapon_single)
+        {
+            if (null == _player.weapon)
+                text.text = "-";
+            else
+            {
+                if (_player.weapon.inf_ammo)
+                    text.text = "Inf Single";
+                else
+                    text.text = _player.weapon.ammo.ToString() + " Single";
+            }
+        }
+    }
 }
